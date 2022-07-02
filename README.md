4589d3f7f4979154e6e49977ca224f4cd53c425f
lidar <97009593+lidarbtc@users.noreply.github.com>
2022-07-02 22:51:07 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# IP-grabber
-IP address and User agent grabber made with flask
+<div align=center>
+ 
+# IP grabber
+ <p>
+ <img src="https://img.shields.io/github/stars/lidarbtc/IP-grabber?color=%23DF0067&style=for-the-badge"/> &nbsp;
+ <img src="https://img.shields.io/github/forks/lidarbtc/IP-grabber?color=%239999FF&style=for-the-badge"/> &nbsp;
+ <img src="https://img.shields.io/github/license/lidarbtc/IP-grabber?color=%23E8E8E8&style=for-the-badge"/> &nbsp;
+ 
+Grab IP and Useragent with flask
+
+## Language</br>
+
+ <img src="https://img.shields.io/badge/Python-FFDD00?style=for-the-badge&logo=python&logoColor=blue"/></br>
+</div>
+
+## Usage on Linux
+```sh
+ pip3 install flask
+ 
+ git clone https://github.com/lidarbtc/IP-grabber.git
+ cd dongbaek-life
+
+ python3 app.py
+ ```
+
+## Contact Developer
+```sh
+ session : 05c67e3461a3b17896f063535c2ade0b91639be688a6de6d5665258995f0fec660
+```
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import datetime
+import flask
+import pymysql
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = b'{Q>GOR(76X^ys!H.V#x:<@HQJRpg{P7)QMKCe]SUTJDe%/aPUN'
+app.config["PERMANENT_SESSION_LIFETIME"] = datetime.timedelta(minutes=60)
+
+HTML_PATH_MAIN = './main.html'
+
+def db_connector(sql_command):
+    MYSQL_DB = {
+        'user'     : 'dbuser',
+        'password' : 'abcd1234',
+        'host'     : 'localhost',
+        'port'     : '3306',
+        'database' : 'grabber'
+    }
+    db = pymysql.connect(
+        host=MYSQL_DB['host'],
+        port=int(MYSQL_DB['port']),
+        user=MYSQL_DB['user'],
+        passwd=MYSQL_DB['password'],
+        db=MYSQL_DB['database'],
+        charset='utf8'
+    )
+    cursor = db.cursor()
+    cursor.execute(sql_command)
+    result = cursor.fetchall()
+    db.commit()
+    db.close()
+    return str(result).replace("(", "").replace(")", "").replace("'", "").replace(',', '').rstrip()
+
+@app.route('/', methods=['GET', 'POST']) 
+def index():
+    ip_address = flask.request.remote_addr
+    useragent = request.headers.get('User-Agent')
+    db_connector(f"INSERT IGNORE INTO info(userip, useragent) VALUES('{ip_address}', '{useragent}');")
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(host="localhost", port="3000",debug=False, threaded=True)
